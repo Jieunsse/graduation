541e39dd04717e45899779fdec87249dccdc174c
jieunsse <jieunsse@gmail.com>
2025-09-14 23:47:29 +0900

    feat: 푸터 디자인 완료

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@shared/ui/header/Header.tsx';
+import Footer from '@shared/ui/footer/Footer.tsx';
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
