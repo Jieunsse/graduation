a24b14a44c5c4dfb50d72b239feb8cdaf9cddd59
jieunsse <jieunsse@gmail.com>
2025-09-14 23:03:55 +0900

    feat : 헤더 디자인 완료

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import 'normalize.css';
 import { Router } from '@app/routes/Router.tsx';
 import '@radix-ui/themes/styles.css';
 import { Theme, ThemePanel } from '@radix-ui/themes';
+import '@shared/styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme accentColor="iris">
+    <Theme accentColor="iris" appearance="dark">
       <Router />
       <ThemePanel />
     </Theme>
--- src/shared/ui/header/Header.tsx
+++ src/shared/ui/header/Header.tsx
@@ -1,17 +1,26 @@
-import { Container, Flex } from '@radix-ui/themes';
+import { Flex } from '@radix-ui/themes';
 import style from './header.module.css';
 
 export const Header = () => {
   return (
-    <>
-      <Container size="1" className={style.container}>
-        <Flex gap="3" align="center" justify="between">
-          <button className={style.buttonStyle}>게시판</button>
-          <button className={style.buttonStyle}>타임라인</button>
-          <button className={style.buttonStyle}>데이터랩</button>
-          <button className={style.buttonStyle}>튜토리얼</button>
+    <div className={style.box}>
+      <div className={style.container}>
+        <Flex align="center" justify="between" className={style.headerRow}>
+          {/* 왼쪽: 로고 */}
+          <span className={style.logo}>F1 KOREA</span>
+
+          {/* 가운데: 메뉴 버튼 */}
+          <Flex gap="6" align="center" justify="center">
+            <button className={style.buttonStyle}>게시판</button>
+            <button className={style.buttonStyle}>타임라인</button>
+            <button className={style.buttonStyle}>데이터랩</button>
+            <button className={style.buttonStyle}>튜토리얼</button>
+          </Flex>
+
+          {/* 오른쪽: 로그인 */}
+          <button className={style.loginButtonStyle}>로그인</button>
         </Flex>
-      </Container>
-    </>
+      </div>
+    </div>
   );
 };
