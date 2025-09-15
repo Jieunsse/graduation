4144e7baa100e3218abd4339e4bb04eb5326c0ac
jieunsse <jieunsse@gmail.com>
2025-09-15 18:12:52 +0900

    chore: 홈 컴포넌트 초기화

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,3 @@
-import { Header } from '@shared/ui/header/Header.tsx';
-import Footer from '@shared/ui/footer/Footer.tsx';
-
 export const Home = () => {
-  return (
-    <>
-      <Header />
-      <Footer />
-    </>
-  );
+  return <>Home</>;
 };
