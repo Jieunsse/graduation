6a704382e75464fc0f89cbfca19706d32e0739a8
jieunsse <jieunsse@gmail.com>
2025-09-15 21:30:54 +0900

    chore : 실행 환경 정리

--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -1,13 +1,15 @@
 import { Header } from '@shared/ui/header/Header.tsx';
 import { Footer } from '@shared/ui/footer/Footer.tsx';
 import { CardContainer } from '@shared/ui/cardContainer/CardContainer.tsx';
+import { SideBar } from '@shared/ui/sideBar/SideBar.tsx';
 
 export const Test = () => {
   return (
     <>
-      <Header />
-      <CardContainer />
-      <Footer />
+      {/*<Header />*/}
+      {/*<CardContainer />*/}
+      {/*<Footer />*/}
+      <SideBar />
     </>
   );
 };
