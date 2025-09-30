2e928614bae3fdb540c0bf7eda32bd269ad9b75c
jieunsse <jieunsse@gmail.com>
2025-09-30 13:38:04 +0900

    chore : 히어로 섹션 안내문구 수정

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -12,11 +12,12 @@ export const Hero = () => {
   return (
     <section className={styles.hero}>
       <h1 className={styles.title}>
-        올인원 포뮬러 1 <br /> 라이브타이밍 대시보드
+        F1 Korea <br /> 커뮤니티
       </h1>
       <p className={styles.description}>
-        실시간 F1 데이터, 라이브 텔레메트리 및 레이스 분석을 하나의 강력한
-        대시보드에서 경험하세요. 이전과는 다른 방식으로 F1을 느낄 수 있습니다.
+        실시간 F1 데이터, 초보자를 위한 용어 설명을 통해 F1 경기를 관람해보세요.
+        <br />
+        이전과는 다른 방식으로 F1을 느낄 수 있습니다.
       </p>
       <Button
         className={styles.ctaButton}
@@ -24,7 +25,7 @@ export const Hero = () => {
         variant="solid"
         onClick={handleClick}
       >
-        Enter the Cosmos
+        Enter the F1 KOREA
       </Button>
     </section>
   );
